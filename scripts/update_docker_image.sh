7838906fa3d75df51150484aa0fbd7c957b3cda5
Ubuntu <ubuntu@ip-172-31-41-114.ap-northeast-1.compute.internal>
2021-02-21 09:08:50 +0000

    get instance region

--- scripts/update_docker_image.sh
+++ scripts/update_docker_image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd `dirname $0`
+AWS_DEFAULT_REGION=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/.$//g'`
 $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
 docker-compose -f ../docker-compose.yml -f ../docker-compose.prod.yml pull
