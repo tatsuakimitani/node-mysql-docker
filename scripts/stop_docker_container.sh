24a11f5f435d0886126cd8b13e32da036780267f
Ubuntu <ubuntu@ip-172-31-41-114.ap-northeast-1.compute.internal>
2021-02-21 07:54:35 +0000

    comment out

--- scripts/stop_docker_container.sh
+++ scripts/stop_docker_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker-compose -f /opt/node-app/docker-compose.yml -f /opt/node-app/docker-compose.prod.yml down --rmi all --volumes --remove-orphans
-rm -rf /opt/node-app/*
+#rm -rf /opt/node-app/*
 
